27929c51055fa0b2f86a9d608d7364d05cd7a3e9
Tolentino-JP <johnpaultolentino5@gmail.com>
2024-03-24 08:49:47 +0800

    Median Line 25 error cannot convert to double

--- MeanMedianMode/Median.cs
+++ MeanMedianMode/Median.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 
 namespace MeanMedianModeBusLogic
 {
@@ -10,23 +11,25 @@ namespace MeanMedianModeBusLogic
     {
         Main main = new Main();
         int count = 0;
+        double total = 0;
         public double Prelim()
         {
-
             var prelimGrade = (from prelim in main.studentNameGrade
-                              select prelim).ToArray();
+                              select prelim);
 
             count = prelimGrade.Count();
-            double arrprelim = new double[]
+            
 
             if(count % 2 == 0)
             {
-                count = prelimGrade;
+                double mid = Convert.ToDouble(prelimGrade.ElementAt(count / 2).ToString() );
+                double midPlus = Convert.ToDouble(prelimGrade.ElementAt( (count/2) +1).ToString() );
+                total = (mid + midPlus) / 2;
             }
 
 
 
-            return 0;
+            return total;
         }
 
 
--- MeanMedianModeConsole/Program.cs
+++ MeanMedianModeConsole/Program.cs
@@ -1,10 +1,11 @@
 ﻿using MeanMedianModeBusLogic;
 
 Mean mean = new Mean();
+Median median = new Median();
 
 
-Console.WriteLine($"Prelim Mean: {Math.Round(mean.Prelim(), 2)}");
-Console.WriteLine($"Midterm Mean: {Math.Round(mean.Midterm(),2)}");
+//Console.WriteLine($"Prelim Mean: {Math.Round(mean.Prelim(), 2)}");
+//Console.WriteLine($"Midterm Mean: {Math.Round(mean.Midterm(),2)}");
 Console.WriteLine($"Finals Mean: {Math.Round(mean.Finals(),2)}");
 
-
+Console.WriteLine($"Prelim Median: {median.Prelim()}");
